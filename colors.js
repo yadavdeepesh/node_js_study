66b29a61d79e6d6872d6f3f806176e5529945431
Deepesh Yadav <yadavdeepesh@13gmail.com>
2025-01-12 13:53:28 +0530

    this code for using color package

---
+++ colors.js
@@ -0,0 +1,5 @@
+const Color = require('color');
+
+const myColor = Color('red'); // Create a color object with 'red'
+
+console.log("Hex value of the color is:", myColor.hex());
--- core_module.js
+++ core_module.js
@@ -1,8 +1,8 @@
 
 console.log("code step by step");
 // const fs = require('fs');//import the all file 
-const fs = require('fs').writeFileSync;// import only single function 
-fs("abc2.txt","this file create by deepesh for code");
+// const fs = require.writeFileSync;// import only single function 
+// fs("abc2.txt","this file create by deepesh for code");
 // this code for create file
 
 console.log("->>",__filename);
