816fd94cf90f0b71814379e1ad219d10bf8642d9
Deepesh Yadav <yadavdeepesh@13gmail.com>
2025-01-18 15:59:10 +0530

    create the api call server and console input

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+{name:'deepesh yadav',email:'dk@gmail.com',contact:99999},
+{name:'shubham sharma',email:'ss@gmail.com'},
+{name:'Abhishek seaxna',email:'as@gmail.com'}
+];
+module.exports=data;
